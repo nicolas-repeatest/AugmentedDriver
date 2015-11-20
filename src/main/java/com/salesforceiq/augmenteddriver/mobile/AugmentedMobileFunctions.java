4a72e71e25274763bc09cf53d1b584c0eb30277a
Fernando <fernando.martin@relateiq.com>
2015-11-20 10:12:46 -0800

    new functionality mobile

--- src/main/java/com/salesforceiq/augmenteddriver/mobile/AugmentedMobileFunctions.java
+++ src/main/java/com/salesforceiq/augmenteddriver/mobile/AugmentedMobileFunctions.java
@@ -1,11 +1,50 @@
 package com.salesforceiq.augmenteddriver.mobile;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * Augmented functionality for Mobile.
  */
 public interface AugmentedMobileFunctions<T> {
+
+    /**
+     * Taps on an element that is not on top (so it is not visible or displayed but it is present).
+     *
+     * @param by the element to be tapped.
+     * @return the element to be tapped (but might not be visible or displayed, but present)
+     */
+    T tap(By by);
+
+    /**
+     * Taps on an element that is not on top (so it is not visible or displayed but it is present).
+     * Waits up to waitTimeInSeconds for the element to be visible.
+     */
+    T tapAfter(By by, int waitTimeInSeconds);
+
+
+    /**
+     * Taps on the offset of an element.
+     *
+     * <p>
+     *     I.e if we select (by, 5, 5) it will tap 5 pixels to the right and 5 pixels to the bottom
+     * </p>
+     */
+    T tap(By by, int offsetX, int offsetY);
+
+    /**
+     * Taps on the offset of an element.
+     *
+     * <p>
+     *     I.e if we select (by, 5, 5) it will tap 5 pixels to the right and 5 pixels to the bottom
+     * </p>
+     */
+    T tapAfter(By by, int offsetX, int offsetY, int waitTimeInSeconds);
+
+    /**
+     * Taps on an element for the given milli seconds
+     */
+    void tap(WebElement element, int pressInMilliSeconds);
 
     /**
      * Clicks on an element (waiting default time to show up), types and then hides the keyboard.

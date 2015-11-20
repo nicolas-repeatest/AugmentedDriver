402d052350061a20bdde409104ff8ffb9905d524
Fernando <fernando.martin@relateiq.com>
2015-11-20 14:16:21 -0800

    upload sacue labs

--- src/main/java/com/salesforceiq/augmenteddriver/integrations/SauceLabsIntegration.java
+++ src/main/java/com/salesforceiq/augmenteddriver/integrations/SauceLabsIntegration.java
@@ -52,10 +52,11 @@ public class SauceLabsIntegration implements Integration {
     /**
      * Uploads a file to SauceLabs. Usually for an app or an apk.
      */
-    public void uploadFile(Path fileToUpload) throws IOException {
+    public void uploadFile(Path fileToUpload, String destinationFileName) throws IOException {
         Preconditions.checkNotNull(fileToUpload);
         Preconditions.checkArgument(Files.exists(fileToUpload));
-        uploadFile(fileToUpload, true);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(destinationFileName));
+        uploadFile(fileToUpload, destinationFileName, true);
     }
 
     /**
@@ -63,10 +64,11 @@ public class SauceLabsIntegration implements Integration {
      *
      * @param overwrite if set to true, it overwrites the file if it exists in SauceLabs.
      */
-    public void uploadFile(Path fileToUpload, boolean overwrite) throws IOException {
+    public void uploadFile(Path fileToUpload, String destinationFileName, boolean overwrite) throws IOException {
         Preconditions.checkNotNull(fileToUpload);
         Preconditions.checkArgument(Files.exists(fileToUpload));
-        sauceRest.uploadFile(fileToUpload.toFile(), fileToUpload.getFileName().toString(), overwrite);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(destinationFileName));
+        sauceRest.uploadFile(fileToUpload.toFile(), destinationFileName, overwrite);
     }
 
     private void update(String key, Object value, String sessionId) {

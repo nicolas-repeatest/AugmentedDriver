70d195465fd378da70c9296b89d4b4e1ed18a365
Fernando <fernando.martin@relateiq.com>
2015-11-23 10:07:25 -0800

    augmented ios and android functions

---
+++ src/main/java/com/salesforceiq/augmenteddriver/mobile/android/AugmentedAndroidFunctionsFactory.java
@@ -0,0 +1,7 @@
+package com.salesforceiq.augmenteddriver.mobile.android;
+
+/**
+ * Created by fernando.martin on 11/23/15.
+ */
+public class AugmentedAndroidFunctionsFactory {
+}
--- src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugomentedIOSFunctionsFactory.java
+++
@@ -1,7 +0,0 @@
-package com.salesforceiq.augmenteddriver.mobile.ios;
-
-/**
- * Created by fernando.martin on 11/11/15.
- */
-public class AugomentedIOSFunctionsFactory {
-}

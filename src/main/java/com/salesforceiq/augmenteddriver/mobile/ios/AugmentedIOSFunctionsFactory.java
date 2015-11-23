b1a2be8f2835b3a134d928dfeb2976feb84d32c6
Fernando <fernando.martin@relateiq.com>
2015-11-23 10:07:25 -0800

    augmented ios and android functions

---
+++ src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugmentedIOSFunctionsFactory.java
@@ -0,0 +1,7 @@
+package com.salesforceiq.augmenteddriver.mobile.ios;
+
+import org.openqa.selenium.SearchContext;
+
+public interface AugmentedIOSFunctionsFactory {
+    AugmentedIOSFunctions create(SearchContext context);
+}

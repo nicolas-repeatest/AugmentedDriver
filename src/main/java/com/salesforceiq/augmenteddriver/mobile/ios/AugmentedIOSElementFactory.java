d74d7e3b0cf5c81cb47656fd1559538a68320bce
Fernando <fernando.martin@relateiq.com>
2016-01-20 10:59:13 -0800

    more javadocs

--- src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugmentedIOSElementFactory.java
+++ src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugmentedIOSElementFactory.java
@@ -2,6 +2,16 @@ package com.salesforceiq.augmenteddriver.mobile.ios;
 
 import org.openqa.selenium.WebElement;
 
+/**
+ * Guice Factory.
+ */
 public interface AugmentedIOSElementFactory {
+
+    /**
+     * Creates an AugmentedIOSElement based on a generic WebElement.
+     *
+     * @param webElement the Generic WebElement.
+     * @return the AugmentedIOSElement that wraps the WebElement.
+     */
     AugmentedIOSElement create(WebElement webElement);
 }
--- src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugmentedIOSFunctions.java
+++ src/main/java/com/salesforceiq/augmenteddriver/mobile/ios/AugmentedIOSFunctions.java
@@ -18,9 +18,12 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation of the augmented mobile functionality for IOS.
+ */
 public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSElement>,
-                                             AugmentedMobileFunctions<AugmentedIOSElement>,
-        AugmentedIOSOnlyFunctions {
+                                              AugmentedMobileFunctions<AugmentedIOSElement>,
+                                              AugmentedIOSOnlyFunctions {
 
     private final int waitTimeInSeconds;
     private final AugmentedIOSElementFactory augmentedIOSElementFactory;
@@ -50,12 +53,14 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public boolean isElementPresent(By by) {
         Preconditions.checkNotNull(by);
+
         return isElementPresentAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public boolean isElementPresentAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         try {
             findElementPresentAfter(by, waitSeconds);
             return true;
@@ -67,18 +72,21 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public boolean isElementPresentImmediate(By by) {
         Preconditions.checkNotNull(by);
-        return isElementPresentAfter(by, 0);
+
+        return isElementPresentAfter(by, 1);
     }
 
     @Override
     public boolean isElementVisible(By by) {
         Preconditions.checkNotNull(by);
+
         return isElementVisibleAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public boolean isElementVisibleAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         try {
             findElementsVisibleAfter(by, waitSeconds);
             return true;
@@ -90,18 +98,21 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public boolean isElementVisibleImmediate(By by) {
         Preconditions.checkNotNull(by);
-        return isElementVisibleAfter(by, 0);
+
+        return isElementVisibleAfter(by, 1);
     }
 
     @Override
     public boolean isElementClickable(By by) {
         Preconditions.checkNotNull(by);
+
         return isElementClickableAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public boolean isElementClickableAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         try {
             findElementClickableAfter(by, waitSeconds);
             return true;
@@ -113,59 +124,71 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public boolean isElementClickableImmediate(By by) {
         Preconditions.checkNotNull(by);
-        return isElementClickableAfter(by, 0);
+
+        return isElementClickableAfter(by, 1);
     }
 
     @Override
     public AugmentedIOSElement findElementPresent(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementPresentAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public AugmentedIOSElement findElementPresentAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.findElementPresentAfter(searchContext, by, waitSeconds));
     }
 
     @Override
     public AugmentedIOSElement findElementVisible(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementVisibleAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public AugmentedIOSElement findElementVisibleAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.findElementVisibleAfter(searchContext, by, waitSeconds));
     }
 
     @Override
     public AugmentedIOSElement findElementClickable(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementClickableAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public AugmentedIOSElement findElementClickableAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.findElementClickableAfter(searchContext, by, waitSeconds));
     }
 
     @Override
     public AugmentedIOSElement findElementNotMoving(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementNotMovingAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public AugmentedIOSElement findElementNotMovingAfter(By by, int waitSeconds) {
         Preconditions.checkNotNull(by);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.findElementNotMovingAfter(searchContext, by, waitSeconds));
     }
 
     @Override
     public AugmentedIOSElement findElementContain(By by, String text) {
+        Preconditions.checkNotNull(by);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(text));
+
         return findElementContainAfter(by, text, waitTimeInSeconds);
     }
 
@@ -173,18 +196,21 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     public AugmentedIOSElement findElementContainAfter(By by, String text, int waitInSeconds) {
         Preconditions.checkNotNull(by);
         Preconditions.checkArgument(!Strings.isNullOrEmpty(text));
+
         return augmentedIOSElementFactory.create(WebDriverUtil.findElementContainAfter(searchContext, by, text, waitInSeconds));
     }
 
     @Override
     public List<AugmentedIOSElement> findElementsVisible(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementsVisibleAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public List<AugmentedIOSElement> findElementsVisibleAfter(By by, int waitInSeconds) {
         Preconditions.checkNotNull(by);
+
         return WebDriverUtil.findElementsVisibleAfter(searchContext, by, waitInSeconds)
                 .stream()
                 .map(augmentedIOSElementFactory::create)
@@ -194,12 +220,14 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public List<AugmentedIOSElement> findElementsPresent(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementsPresentAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public List<AugmentedIOSElement> findElementsPresentAfter(By by, int waitInSeconds) {
         Preconditions.checkNotNull(by);
+
         return WebDriverUtil.findElementsPresentAfter(searchContext, by, waitInSeconds)
                 .stream()
                 .map(augmentedIOSElementFactory::create)
@@ -209,12 +237,14 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public List<AugmentedIOSElement> findElementsClickable(By by) {
         Preconditions.checkNotNull(by);
+
         return findElementsClickableAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public List<AugmentedIOSElement> findElementsClickableAfter(By by, int waitInSeconds) {
         Preconditions.checkNotNull(by);
+
         return WebDriverUtil.findElementsClickableAfter(searchContext, by, waitInSeconds)
                 .stream()
                 .map(augmentedIOSElementFactory::create)
@@ -223,28 +253,37 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void waitElementToNotBePresent(By by) {
+        Preconditions.checkNotNull(by);
+
         waitElementToNotBePresentAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public void waitElementToNotBePresentAfter(By by, int waitInSeconds) {
         Preconditions.checkNotNull(by);
+
         WebDriverUtil.waitElementToNotBePresent(searchContext, by, waitInSeconds);
     }
 
     @Override
     public void waitElementToNotBeVisible(By by) {
+        Preconditions.checkNotNull(by);
+
         waitElementToNotBeVisibleAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public void waitElementToNotBeVisibleAfter(By by, int waitInSeconds) {
         Preconditions.checkNotNull(by);
+
         WebDriverUtil.waitElementToNotBeVisible(searchContext, by, waitInSeconds);
     }
 
     @Override
     public AugmentedIOSElement clickAndPresent(By click, By wait) {
+        Preconditions.checkNotNull(click);
+        Preconditions.checkNotNull(wait);
+
         return clickAndPresentAfter(click, wait, waitTimeInSeconds);
     }
 
@@ -252,32 +291,46 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     public AugmentedIOSElement clickAndPresentAfter(By click, By wait, int waitInSeconds) {
         Preconditions.checkNotNull(click);
         Preconditions.checkNotNull(wait);
+
         findElementClickableAfter(click, waitInSeconds).click();
         return findElementPresentAfter(wait, waitInSeconds);
     }
 
     @Override
     public void moveToAndClick(By moveTo, By click) {
+        Preconditions.checkNotNull(click);
+        Preconditions.checkNotNull(moveTo);
+
         WebDriverUtil.moveToAndClick(augmentedIOSDriverProvider.get(), moveTo, click, waitTimeInSeconds);
     }
 
     @Override
     public void moveToAndClickAfter(By moveTo, By click, int waitInSeconds) {
+        Preconditions.checkNotNull(click);
+        Preconditions.checkNotNull(moveTo);
+
         WebDriverUtil.moveToAndClick(augmentedIOSDriverProvider.get(), moveTo, click, waitInSeconds);
     }
 
     @Override
     public AugmentedIOSElement moveTo(By moveTo) {
+        Preconditions.checkNotNull(moveTo);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.moveTo(augmentedIOSDriverProvider.get(), moveTo, waitTimeInSeconds));
     }
 
     @Override
     public AugmentedIOSElement moveToAfter(By moveTo, int waitInSeconds) {
+        Preconditions.checkNotNull(moveTo);
+
         return augmentedIOSElementFactory.create(WebDriverUtil.moveTo(augmentedIOSDriverProvider.get(), moveTo, waitInSeconds));
     }
 
     @Override
     public void clearAndSendKeys(By by, String text) {
+        Preconditions.checkNotNull(by);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(text));
+
         clearAndSendKeysAfter(by, text, waitTimeInSeconds);
     }
 
@@ -285,6 +338,7 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     public void clearAndSendKeysAfter(By by, String text, int waitInSeconds) {
         Preconditions.checkNotNull(by);
         Preconditions.checkArgument(!Strings.isNullOrEmpty(text));
+
         AugmentedIOSElement element = findElementClickableAfter(by, waitInSeconds);
         element.clear();
         element.sendKeys(text);
@@ -293,12 +347,14 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     @Override
     public AugmentedIOSElement tap(By by) {
         Preconditions.checkNotNull(by);
+
         return tapAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public AugmentedIOSElement tapAfter(By by, int waitTimeInSeconds) {
         Preconditions.checkNotNull(by);
+
         WebElement element = MobileUtil.tapAfter(
                 augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
@@ -378,7 +434,8 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     }
 
     @Override
-    public AugmentedIOSElement tapCustomAfter(By by, int offsetX, int offsetY, int pressInMilliSeconds, int fingers, int waitTimeInSeconds) {
+    public AugmentedIOSElement tapCustomAfter(By by, int offsetX, int offsetY, int pressInMilliSeconds,
+                                              int fingers, int waitTimeInSeconds) {
         Preconditions.checkNotNull(by);
 
         WebElement element = MobileUtil.tapAfter(augmentedIOSDriverProvider.get(),
@@ -402,17 +459,26 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void clickAndSendKeys(By by, String keys) {
+        Preconditions.checkNotNull(by);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(keys));
+
         clickAndSendKeysAfter(by, keys, waitTimeInSeconds);
     }
 
     @Override
     public void clickAndSendKeysAfter(By by, String keys, int waitInSeconds) {
+        Preconditions.checkNotNull(by);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(keys));
+
         findElementClickableAfter(by, waitInSeconds).click();
         findElementClickableAfter(by, waitInSeconds).sendKeys(keys);
     }
 
     @Override
     public AugmentedIOSElement swipeUpWaitElementVisible(By swipeUpElement, By elementPresent) {
+        Preconditions.checkNotNull(swipeUpElement);
+        Preconditions.checkNotNull(elementPresent);
+
         WebElement element = MobileUtil.swipeUpWaitVisibleAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeUpElement,
@@ -425,6 +491,9 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public AugmentedIOSElement swipeDownWaitElementVisible(By swipeDownElement, By elementPresent) {
+        Preconditions.checkNotNull(swipeDownElement);
+        Preconditions.checkNotNull(elementPresent);
+
         WebElement element = MobileUtil.swipeDownWaitVisible(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeDownElement,
@@ -436,7 +505,11 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
     }
 
     @Override
-    public AugmentedIOSElement swipeVerticalWaitVisible(By swipeElement, By elementVisible, int offset, int quantity, int durationInMilliSeconds) {
+    public AugmentedIOSElement swipeVerticalWaitVisible(By swipeElement, By elementVisible,
+                                                        int offset, int quantity, int durationInMilliSeconds) {
+        Preconditions.checkNotNull(swipeElement);
+        Preconditions.checkNotNull(elementVisible);
+
         WebElement element = MobileUtil.swipeVerticalWaitVisibleAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeElement,
@@ -450,6 +523,8 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void swipeUp(By swipeBy) {
+        Preconditions.checkNotNull(swipeBy);
+
         MobileUtil.swipeUpAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeBy,
@@ -459,6 +534,8 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void swipeDown(By swipeBy) {
+        Preconditions.checkNotNull(swipeBy);
+
         MobileUtil.swipeDownAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeBy,
@@ -468,6 +545,8 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void swipeVertical(By swipeBy, int offset, int durationInMilliSeconds) {
+        Preconditions.checkNotNull(swipeBy);
+
         MobileUtil.swipeVerticalAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 swipeBy,
@@ -479,11 +558,15 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void swipeFullRight(By by) {
+        Preconditions.checkNotNull(by);
+
         swipeFullRightAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public void swipeFullRightAfter(By by, int waitInSeconds) {
+        Preconditions.checkNotNull(by);
+
         MobileUtil.swipeFullRightAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 by,
@@ -493,11 +576,15 @@ public class AugmentedIOSFunctions implements AugmentedFunctions<AugmentedIOSEle
 
     @Override
     public void swipeFullLeft(By by) {
+        Preconditions.checkNotNull(by);
+
         swipeFullLeftAfter(by, waitTimeInSeconds);
     }
 
     @Override
     public void swipeFullLeftAfter(By by, int waitInSeconds) {
+        Preconditions.checkNotNull(by);
+
         MobileUtil.swipeFullLeftAfter(augmentedIOSDriverProvider.get(),
                 augmentedIOSDriverProvider.get().augmented(),
                 by,
